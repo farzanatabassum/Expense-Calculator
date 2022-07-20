570f4be9f89f0f70be9011f0a48804bcb9275d86
farzanatabassum <82896277+farzanatabassum@users.noreply.github.com>
2022-07-20 10:53:38 +0600

    function reuse

--- app.js
+++ app.js
@@ -1,60 +1,70 @@
+//for income
+function getIncomeValue(){
+    const incomeText=document.getElementById('income');
+    const income=parseFloat(incomeText.value);
+    return income;
+}
+//for expenses
+function getExpenseValue(expenseId){
+    const expenseText=document.getElementById(expenseId)
+    const expense=parseFloat(expenseText.value);
+    return expense;
+}
+//for total expense
+function expenseTotalValue(id){
+    //for food
+    const food=getExpenseValue('food');
+    //for rent
+    const rent=getExpenseValue('rent');
+    //for clothes
+    const cloth=getExpenseValue('cloth');
+    //total expense
+    const result=food+rent+cloth;
+    const total=document.getElementById(id);
+    total.innerText=result;
+    return result;
+
+}
+//for balance
+function updateBalance(id){
+    const result=expenseTotalValue('total');
+    const left=getIncomeValue()-result;
+    const balance=document.getElementById(id);
+    balance.innerText=left;
+    const balanceTotal=balance.innerText;
+    return balanceTotal;
+
+}
 // for calculate button
 document .getElementById("calculate-btn").addEventListener('click',function(){
-//for income
-const incomeText=document.getElementById('income');
-const income=parseFloat(incomeText.value);
-//for food
-const foodText=document.getElementById('food');
-const food=parseFloat(foodText.value);
-//for rent
-const rentText=document.getElementById("rent");
-const rent=parseFloat(rentText.value);
-//for clothes
-const clothText=document.getElementById('cloth');
-const cloth=parseFloat(clothText.value);
-const result=food+rent+cloth;
-//total expenses
-const total=document.getElementById('total');
-total.innerText=result;
-//Balance
-const left=income-result;
-const balance=document.getElementById("balance");
-balance.innerText=left;
+    //for income
+    const income=getIncomeValue();
+
+    //Balance
+    updateBalance('balance');
+
+
 })
+
+
 //for save button
 document.getElementById("save-btn").addEventListener('click',function(){
     //for income
-const incomeText=document.getElementById('income');
-const income=parseFloat(incomeText.value);
-const save=document.getElementById("save");
-const saveValue=parseInt(save.value);
-const percentage=parseFloat(saveValue/100);
-const savingTotal=income*percentage;
-//Saving amount
-const saveText=document.getElementById("save-amount");
-saveText.innerText=parseFloat(savingTotal);
-
-const saveTotal=saveText.innerText;
-//remaining balance
-//for food
-const foodText=document.getElementById('food');
-const food=parseFloat(foodText.value);
-//for rent
-const rentText=document.getElementById("rent");
-const rent=parseFloat(rentText.value);
-//for clothes
-const clothText=document.getElementById('cloth');
-const cloth=parseFloat(clothText.value);
-const result=food+rent+cloth;
-//total expenses
-const total=document.getElementById('total');
-total.innerText=result;
-const left=income-result;
-const balance=document.getElementById("balance");
-balance.innerText=left;
-
-const remain=document.getElementById('remain');
-remain.innerText=balance.innerText-saveTotal;
+    const income=getIncomeValue();
+    //for save button
+    const save=document.getElementById("save");
+    const saveValue=parseInt(save.value);
+    const percentage=parseFloat(saveValue/100);
+    const savingTotal=income*percentage;
+    //Saving amount
+    const saveText=document.getElementById("save-amount");
+    saveText.innerText=parseFloat(savingTotal);
+    const saveTotal=saveText.innerText;
+    //total expenses
+    const balanceTotal=updateBalance('balance');
+    //remaining balance
+    const remain=document.getElementById('remain');
+    remain.innerText=balanceTotal-saveTotal;
 
 
 
